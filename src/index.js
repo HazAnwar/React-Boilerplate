3d072f6b9fb00f8e2fcfab211269318f05131e2a
HazAnwar <3630449+HazAnwar@users.noreply.github.com>
2020-02-19 18:34:26 +0000

    remove unused items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import "shards-ui/dist/css/shards.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
